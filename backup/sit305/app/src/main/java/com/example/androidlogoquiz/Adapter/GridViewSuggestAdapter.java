00755c561f44690492cd7341f70152a89bf6342c
DARKEUNG <hawygfau@gmail.com>
2019-04-05 12:10:47 +1100

    App able to run
    The app is being completed, the app is able to run.
    
    The next step is to debug the software.
    
    After debug will do the index page.
    
    Anders Please help me to fill in SID IN README(INDEX) & WHAT'S THIS APP DOING in README(UNDER BACKUP FILE) & SOME DOCUMENTATION. THANKS!
    
    Co-Authored-By: Anders kuang <kuanjianchen@users.noreply.github.com>

--- backup/sit305/app/src/main/java/com/example/androidlogoquiz/Adapter/GridViewSuggestAdapter.java
+++ backup/sit305/app/src/main/java/com/example/androidlogoquiz/Adapter/GridViewSuggestAdapter.java
@@ -8,6 +8,7 @@ import android.widget.BaseAdapter;
 import android.widget.Button;
 import android.widget.GridView;
 
+import com.example.androidlogoquiz.Common.Common;
 import com.example.androidlogoquiz.MainActivity;
 
 import java.util.List;
@@ -18,33 +19,40 @@ public class GridViewSuggestAdapter extends BaseAdapter {
     private Context context;
     private MainActivity mainActivity;
 
-    public GridViewAnswerAdapter(List<String> suggestSource, Context context, MainActivity mainActivity){
+    public GridViewSuggestAdapter(List<String> suggestSource, Context context, MainActivity mainActivity) {
      this.suggestSource = suggestSource;
      this.context = context;
      this.mainActivity = mainActivity;
     }
 
     @Override
-    public  int getCount() {
+    public int getCount() {
         return suggestSource.size();
     }
 
     @Override
-    public Object getItem(int position){
+    public Object getItem(int position) {
         return suggestSource.get(position);
     }
 
     @Override
-    public  long getItemId(int position){
+    public long getItemId(int position) {
         return position;
     }
 
     @Override
-    public View getView(int position, View convertView, ViewGroup parent){
+    public View getView(final int position, View convertView, ViewGroup parent) {
         Button button;
-        if ((convertView == null)
+        if(convertView == null)
         {
-           if (suggestSource.get(position).equals("null"))
+            if(suggestSource.get(position).equals("null"))
+            {
+                button = new Button(context);
+                button.setLayoutParams(new GridView.LayoutParams(85,85));
+                button.setPadding(8,8,8,8);
+                button.setBackgroundColor(Color.DKGRAY);
+            }
+            else
             {
                 button = new Button(context);
                 button.setLayoutParams(new GridView.LayoutParams(85,85));
@@ -55,14 +63,43 @@ public class GridViewSuggestAdapter extends BaseAdapter {
                 button.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
-                        //
+                        //If correct answer contains character user selected
+                        if(String.valueOf(mainActivity.answer).contains(suggestSource.get(position)))
+                        {
+                            char compare = suggestSource.get(position).charAt(0); // Get char
+
+                            for(int i =0;i<mainActivity.answer.length;i++)
+                            {
+                                if(compare == mainActivity.answer[i])
+                                    Common.user_submit_answer[i] = compare;
+                            }
+
+                            //Update UI
+                            GridViewAnswerAdapter answerAdapter = new GridViewAnswerAdapter(Common.user_submit_answer,context);
+                            mainActivity.gridViewAnswer.setAdapter(answerAdapter);
+                            answerAdapter.notifyDataSetChanged();
+
+                            //Remove from suggest source
+                            mainActivity.suggestSource.set(position,"null");
+                            mainActivity.suggestAdapter = new GridViewSuggestAdapter(mainActivity.suggestSource,context,mainActivity);
+                            mainActivity.gridViewSuggest.setAdapter(mainActivity.suggestAdapter);
+                            mainActivity.suggestAdapter.notifyDataSetChanged();
+                        }
+                        else // else
+                        {
+                            //Remove from suggest source
+                            mainActivity.suggestSource.set(position,"null");
+                            mainActivity.suggestAdapter = new GridViewSuggestAdapter(mainActivity.suggestSource,context,mainActivity);
+                            mainActivity.gridViewSuggest.setAdapter(mainActivity.suggestAdapter);
+                            mainActivity.suggestAdapter.notifyDataSetChanged();
+                        }
                     }
                 });
             }
         }
-       else
-           button = (Button)convertView;
-       return  button;
+        else
+            button = (Button)convertView;
+        return button;
 
     }
 }
